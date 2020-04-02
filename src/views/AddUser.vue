c70e09075cc3d7f1f2396f600630d330d257e8bc
singingXY <singeve@qq.com>
2020-04-02 11:02:33 +0800

    验证

--- src/views/AddUser.vue
+++ src/views/AddUser.vue
@@ -67,20 +67,27 @@
                :visible.sync="dialogFormVisible">
       <el-form :model="newUser"
                inline
-               label-width="100px">
-        <el-form-item label="用户名">
+               label-width="100px"
+               :rules="rules"
+               ref="ruleForm">
+        <el-form-item label="用户名"
+                      prop="username">
           <el-input v-model="newUser.username"></el-input>
         </el-form-item>
-        <el-form-item label="手机号">
-          <el-input v-model="newUser.phone"></el-input>
+        <el-form-item label="手机号"
+                      prop="phone">
+          <el-input v-model.number="newUser.phone"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱">
+        <el-form-item label="邮箱"
+                      prop="email">
           <el-input v-model="newUser.email"></el-input>
         </el-form-item>
-        <el-form-item label="公司名">
+        <el-form-item label="公司名"
+                      prop="company">
           <el-input v-model="newUser.company"></el-input>
         </el-form-item>
-        <el-form-item label="行业类型">
+        <el-form-item label="行业类型"
+                      prop="industry">
           <el-input v-model="newUser.industry"></el-input>
         </el-form-item>
       </el-form>
@@ -88,7 +95,7 @@
            class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary"
-                   @click="addUser">确 定</el-button>
+                   @click="addUser('ruleForm')">确 定</el-button>
       </div>
     </el-dialog>
   </div>
@@ -103,7 +110,37 @@ export default {
       userList: [],
       currentPage: 1,
       dialogFormVisible: false, // 添加用户对话框
-      newUser: {}
+      newUser: {},
+      rules: {
+        username: [
+          { required: true, message: "请填写用户名", trigger: "blur" }
+        ],
+        phone: [
+          { required: true, message: "请填写手机号", trigger: "blur" },
+          {
+            validator: function(rule, value, callback) {
+              if (/^1[34578]\d{9}$/.test(value) == false) {
+                callback(new Error("请输入正确的手机号"));
+              } else {
+                callback();
+              }
+            },
+            message: "请输入正确的手机号码"
+          }
+        ],
+        email: [
+          { required: true, message: "请填写邮箱", trigger: "blur" },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
+          }
+        ],
+        company: [{ required: true, message: "请填写公司名", trigger: "blur" }],
+        industry: [
+          { required: true, message: "请填写行业类型", trigger: "blur" }
+        ]
+      }
     };
   },
   created() {
@@ -132,15 +169,22 @@ export default {
       //手机号显示星号
       return row.phone.replace(/(\d{3})\d{4}(\d{4})/, "$1****$2");
     },
-    addUser() {
-      //添加用户
-      apiAddUser(this.newUser).then(res => {
-        if (res.status === 200) {
-          this.userData = res;
-          this.currentPage = parseInt(this.userData.pagination.pageIndex);
-          this.userList = res.data.list;
-          this.dialogFormVisible = false;
-          this.$message("添加成功");
+    addUser(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          //添加用户
+          apiAddUser(this.newUser).then(res => {
+            if (res.status === 200) {
+              this.userData = res;
+              this.currentPage = parseInt(this.userData.pagination.pageIndex);
+              this.userList = res.data.list;
+              this.dialogFormVisible = false;
+              this.$message("添加成功");
+            }
+          });
+        } else {
+          this.$message.error("请正确填写信息");
+          return false;
         }
       });
     }
@@ -215,6 +259,7 @@ export default {
   }
   /deep/ .el-input__inner {
     @include backgroundColor("background_color-2");
+    @include fontColor("font_color-2");
   }
   /deep/ .el-form-item__label {
     @include fontColor("font_color-2");
