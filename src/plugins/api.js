b3f2742755495c5c646d3200c4ea68c5b3ed3d59
singingXY <singeve@qq.com>
2021-04-08 17:26:26 +0800

    pages

--- src/plugins/api.js
+++
@@ -1,9 +0,0 @@
-/**
- * api接口统一管理
- */
-//import { get, post } from './axios'
-import { post } from './axios'
-
-export const apiUserList = (param) => post('/UserList', param)
-export const apiAddUser = (param) => post('/AddUser', param)
-export const apiQA = (param) => post('/qa', param)
